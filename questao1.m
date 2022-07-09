c25d2a19f1ed280342cf4713d117010a3faf4351
patriciapaula <patricia.paula@gmail.com>
2022-07-09 10:56:33 -0300

    testes

--- questao1.m
+++ questao1.m
@@ -14,9 +14,8 @@ r=1;
 [A,B,C,D] = tf2ss(b,a);
 
 % polinomio caracteristico: det(si - A)
-lambda = sym('lambda');
-si = [lambda 0 0;0 lambda 0;0 0 lambda];
-pol = det(si - A);
+si = [s 0 0;0 s 0;0 0 s];
+polc = det(si - A);
 
 % matrix de controlabilidade
 Co = ctrb(A,B);
@@ -27,14 +26,23 @@ nc = length(A) - rank(Co); % 0 -> controlavel
 Obs = obsv(A,C);
 no = length(A) - rank(Obs); % 0 -> observável
 
+% polinomio caracteristico com polos desejados
+% polck = (s+8)*(s+2+2*j)*(s+2-2*j)*(s+4+4*j)*(s+4-4*j);
+polck = s^5 + 20*s^4 + 168*s^3 + 768*s^2 + 1792*s + 2048;
+
+% Kb = [20 167 767 1790 2046]; % calculado a mao
+% K = Kb * Co * inv(Co); % tamanhos nao batem
+
 % [Abar,Bbar,Cbar,P,k] = ctrbf(A,B,C);
 
-% polos desejados - sistema aumentado
-
+% polos desejados - eq estado aumentado
 Al = [A zeros(length(A),1);-C 0]; % horzcat([A;C;zeros(1,3)],zeros(5,2)); % [A zeros(length(A),2);-C 0 0;0 0 0 0 0]
 Bl = [B;0];
 Cl = [C 0];
 El = [zeros(length(A),1);1];
+Col = ctrb(Al,Bl);
+Obsl = obsv(Al,Cl);
+
 newpoles = [-2+2*i -2-2*i -4+4*i -4-4*i];
 k = place(Al,Bl,newpoles);
 % malha fechada com realim estado
@@ -43,9 +51,6 @@ Ecl = eig(Acl);
 
 %N = inv(Cl*inv(-Acl+Bl*k)*Bl);
 
-% polinomio caracteristico desejado
-%pck = (s+8)*(s+2+2*j)*(s+2-2*j)*(s+4+4*j)*(s+4-4*j);
-
 % sistema aberto
 sysop = ss(Al,Bl,Cl,D);
 
@@ -53,12 +58,16 @@ sysop = ss(Al,Bl,Cl,D);
 syscl = ss(Acl,Bl,Cl,D);
 
 figure(1)
+grid
 step(tf(sysop))
+
 figure(2)
+grid
 step(tf(syscl))
 
 figure(3)
 hold on
+grid
 axis
 plot(eig(A),'o','color','b')
 plot(eig(Acl),'*','color','r')
