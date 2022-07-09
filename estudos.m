285a7a7e088a59f6d971486860f5a4c99b835856
Wellington Wagner F. Sarmento <wwagner@virtual.ufc.br>
2022-07-09 10:47:40 -0300

    With Ka and K

--- estudos.m
+++ estudos.m
@@ -13,39 +13,33 @@ r=1;
 
 
 sys = tf(n,d);
-% polso = pole(sys);
-% 
-% %Teste de Observabilidade
-% Obs = obsv(A,C);
-% isobs = length(A)-rank(Obs); % 0 - Sistema eh observavel
-% 
-% %Teste de Controlabilidade
-% [Abar,Bbar,Cbar,T,k] = ctrbf(A,B,C);
 
 %Forma Aumentada
-Al=[A zeros(length(A),1);-C 0];
-Bl = [B;0];
-Cl = [C 0];
-El = [zeros(length(A),1);1];
-disturbance = ones(length(Bl),1)*1.1;
+Aa=[A zeros(length(A),1);-C 0];
+Ba = [B;0];
+Ca = [C 0];
+Ea = [zeros(length(A),1);1];
+% disturbance = ones(length(Bl),1)*1.1;
 
 
 
 %Polos desejados em sistema de Malha Aberta
-sys_o = ss(Al,[Bl El],Cl,0);
-cp=charpoly(Al);
-poles=[-2+2j -2-2j -4+4j -4-4j];
+sys_o = ss(Aa,[Ba Ea],Ca,0);
+poles_d=[-2+2j -2-2j -4+4j -4-4j];
 
 %Realimentacao de Estados
-Kl = place(Al,Bl,poles);
+Ka = place(Aa,Ba,poles_d);
+
+poles=pole(sys);
+K=place(A,B,poles);
 
 %Sistema em Malha Fechada
-Ac = Al-Bl*Kl;
-sys_c = ss(Ac,El,Cl,0);
+Ac = Aa-Ba*Ka;
+sys_c = ss(Ac,Ea,Ca,0);
 
 figure
 subplot(211);
-step(sys_o);
+step(sys);
 title('Resposta a Degrau em Malha Aberta');
 subplot(212);
 step(sys_c);
